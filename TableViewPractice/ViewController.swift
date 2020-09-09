51fe7594e113373dc18e903ef4fd3cf55692fec6
urmil <ucodes28@gmail.com>
2020-09-08 19:36:50 -0500

    Initial Commit

---
+++ TableViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by Urmil Shah on 9/8/20.
+//  Copyright © 2020 Spark28 Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
